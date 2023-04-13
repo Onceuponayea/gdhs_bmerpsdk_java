e11600a6840ba6fc210f729345d6b13ac0f36c1d
onceuponayea <eric.linjialong@gmail.com>
2023-04-13 11:08:09 +0800

    update: add findBanmaerpPropertiesByX_BANMA_MASTER_APP_ACCOUNTAndStatus

--- src/main/java/com/hrghs/xycb/repositories/BanmaerpPropertiesRepository.java
+++ src/main/java/com/hrghs/xycb/repositories/BanmaerpPropertiesRepository.java
@@ -10,6 +10,10 @@ import org.springframework.stereotype.Repository;
 public interface BanmaerpPropertiesRepository extends JpaRepository<BanmaerpProperties,String> {
     //@Query(nativeQuery = true, value = "select * from bmerp_properties where X_BANMA_MASTER_APP_ACCOUNT = :account")
     @Query(nativeQuery = true,value = "select bmerp_properties.* from bmerp_properties left join bmerp_account on " +
-            "bmerp_properties.X_BANMA_MASTER_APP_ID=bmerp_account.banma_master_app_id where bmerp_account.phone = :account")
+            "bmerp_properties.X_BANMA_MASTER_APP_ID=bmerp_account.banma_master_app_id where bmerp_account.phone = :account and bmerp_account.state !='离职' ")
     BanmaerpProperties findBanmaerpPropertiesByX_BANMA_MASTER_APP_ACCOUNT(@Param("account") String account);
+
+    @Query(nativeQuery = true,value = "select bmerp_properties.* from bmerp_properties left join bmerp_account on " +
+            "bmerp_properties.X_BANMA_MASTER_APP_ID=bmerp_account.banma_master_app_id where bmerp_account.phone = :account and bmerp_account.state = :status")
+    BanmaerpProperties findBanmaerpPropertiesByX_BANMA_MASTER_APP_ACCOUNTAndStatus(@Param("account") String account,@Param("status")String status);
 }
